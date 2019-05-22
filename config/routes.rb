04813f3ee36aa1eed26fe91ea1067bb9e3576578
Samprati Kothari <sampratikothari@Sampratis-MacBook-Pro.local>
2019-05-22 16:24:59 +0530

    only one route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@
 
 Spree::Core::Engine.routes.draw do
 	get '/paytm', :to => "paytm#index", :as => :paytm_proceed
-	post '/paytm', :to => "paytm#index", :as => :paytm_proceed
+	# post '/paytm', :to => "paytm#index", :as => :paytm_proceed
   	post '/paytm/confirm', :to => "paytm#confirm", :as => :paytm_confirm
   	post '/paytm/cancel', :to => "paytm#cancel", :as => :paytm_cancel
 end
--- lib/spree_paytm/version.rb
+++ lib/spree_paytm/version.rb
@@ -1,3 +1,3 @@
 module SpreePaytm
-  VERSION = '1.0.3.7'
+  VERSION = '1.0.3.8'
 end
